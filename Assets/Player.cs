c9390ec216a618069346ae2dec6472dd7be308d1
Aashray Kini <aashraykini@gmail.com>
2026-09-12 15:27:09 +0530

    Update player movement and animations

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,22 +1,43 @@
+using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private Animator anim;
     private Rigidbody2D rb;
 
+    [Header("Movement details")]
     [SerializeField] private float moveSpeed = 3.5f;
     [SerializeField] private float jumpForce = 8;
     private float xInput;
+    private bool facingRight = true;
+    private bool isGrounded;
+
+    [Header("Collision details")]
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask whatIsGround;
+
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
     }
 
     private void Update()
     {
+        HandleCollision();
         HandleInput();
         HandleMovement();
+        HandleAnimations();
+        HandleFlip();
+    }
+
+    private void HandleAnimations()
+    {
+        anim.SetFloat("xVelocity", rb.linearVelocity.x);
+        anim.SetFloat("yVelocity", rb.linearVelocity.y);
+        anim.SetBool("isGrounded", isGrounded);
     }
 
     private void HandleInput()
@@ -30,15 +51,43 @@ public class Player : MonoBehaviour
     private void HandleMovement()
     {
         rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocity.y);
-
-        if (xInput > 0)
-            transform.localScale = new Vector3(1.3645f, 1.3645f, 1);
-        else if (xInput < 0)
-            transform.localScale = new Vector3(-1.3645f, 1.3645f, 1);
     }
 
     private void Jump()
     {
-        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        if (isGrounded)
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+    }
+
+    private void HandleCollision()
+    {
+        isGrounded = Physics2D.Raycast(
+            transform.position,
+            Vector2.down,
+            groundCheckDistance,
+            whatIsGround
+        );
+    }
+
+    private void HandleFlip()
+    {
+        if (rb.linearVelocity.x > 0 && facingRight == false)
+            Flip();
+        else if (rb.linearVelocity.x < 0 && facingRight == true)
+            Flip();
+    }
+
+    private void Flip()
+    {
+        transform.Rotate(0, 180, 0);
+        facingRight = !facingRight;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(
+            transform.position,
+            transform.position + new Vector3(0, -groundCheckDistance)
+        );
     }
 }
